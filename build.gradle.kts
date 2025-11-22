d62725322dcdd9c900585281350f245a32cc8623
Chemidios <jopugu@gmail.com>
2025-11-22 04:18:57 +0100

    Refactor

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ java {
 
 repositories {
 	mavenCentral()
+	google()
 }
 
 dependencies {
@@ -25,7 +26,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("com.google.firebase:firebase-crashlytics-buildtools:3.0.6")
+
     runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
